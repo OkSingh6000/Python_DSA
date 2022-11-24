2526a28b9fe3a48939ae268340d61b3cf741e8db
OkSingh6000 <ok.singh6000@gmail.com>
2022-11-25 01:56:17 +0530

    version 1

---
+++ numeric-data.py
@@ -0,0 +1,30 @@
+# Int Data Type...
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+# Float Data Type...
+
+myValue2 = 1.45
+print(myValue2)
+print(type(myValue2))
+print(str(myValue2) + " is of the data type " + str(type(myValue2)))
+
+
+# Complex Data Type.....
+
+myValue3 = 5j
+print(myValue3)
+print(type(myValue3))
+print(str(myValue3) + " is of the data type " + str(type(myValue3)))
+
+
+# Boolean Data Type.....
+
+myValue4 = True
+print(myValue4)
+print(type(myValue4))
+print(str(myValue4) + " is of the data type " + str(type(myValue4)))
+
---
+++ string-data-type.py
@@ -0,0 +1,46 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+# String Concatenation....
+
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+
+# Working with Input strings....
+
+name = input("What is your name? ")
+print(name)
+
+
+# Formatting output strings....
+
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
